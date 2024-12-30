434be3e02df7ef6d4a1015d0a1f8d41f0295ba2e
Priyanshi Bothra <priyanshibothra753@gmail.com>
2024-12-30 15:01:56 +0530

    css updated

---
+++ routes/dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../css/stylesheet.css">
+</head>
+<body>
+    <button>Back</button>
+    <button>Logout</button>
+    <h1>Online Voting System</h1>
+    <hr>
+    <div id="profile"></div>
+    <div id="profile"></div>
+    
+</body>
+</html>
